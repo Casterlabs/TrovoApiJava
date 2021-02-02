4c2f7589fa177c798fd8eff8cb3486baa2194f7b
Ender <33337309+e3ndr@users.noreply.github.com>
2021-02-02 11:14:36 -0600

    1.1.1
    Forgot to register the serializer.

--- src/main/java/co/casterlabs/trovoapi/TrovoApiJava.java
+++ src/main/java/co/casterlabs/trovoapi/TrovoApiJava.java
@@ -3,13 +3,16 @@ package co.casterlabs.trovoapi;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import co.casterlabs.trovoapi.chat.TrovoUserMedal;
 import co.casterlabs.trovoapi.chat.TrovoUserRoles;
+import co.casterlabs.trovoapi.serializers.TrovoUserMedalSerializer;
 import co.casterlabs.trovoapi.serializers.TrovoUserRolesSerializer;
 
 public class TrovoApiJava {
     // @formatter:off
     public static final Gson GSON = new GsonBuilder()
             .registerTypeAdapter(TrovoUserRoles.class, new TrovoUserRolesSerializer())
+            .registerTypeAdapter(TrovoUserMedal.class, new TrovoUserMedalSerializer())
             .create();
     // @formatter:on
 
