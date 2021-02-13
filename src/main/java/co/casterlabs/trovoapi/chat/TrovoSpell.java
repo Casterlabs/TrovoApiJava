61287ba5bafb123a947d17749adb0ab2c91bfdb9
Ender <33337309+e3ndr@users.noreply.github.com>
2021-02-13 14:06:46 -0600

    1.2.2
    Add missing spells.

--- src/main/java/co/casterlabs/trovoapi/chat/TrovoSpell.java
+++ src/main/java/co/casterlabs/trovoapi/chat/TrovoSpell.java
@@ -7,6 +7,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TrovoSpell {
+    JOY("https://img.trovo.live/imgupload/shop/20210208_lihswp6n7tr.png", "Joy", 1000, TrovoSpellCurrency.MANA),
+    CHOCOLATE("https://img.trovo.live/imgupload/shop/20210204_5kbiy6cf4ln.webp", "Chocolate", 100, TrovoSpellCurrency.ELIXIR),
+    LOVE_LETTER("https://img.trovo.live/imgupload/shop/20210205_grn7g2e7ix.webp", "Love Letter", 500, TrovoSpellCurrency.ELIXIR),
+    CUPID_LEON("https://img.trovo.live/imgupload/shop/20210201_f0vs2h1j9e.webp", "Cupid Leon", 2000, TrovoSpellCurrency.ELIXIR),
     RISE_2021("https://img.trovo.live/imgupload/shop/20201230_a0ggwjtmxhg.webp", "RISE 2021", 5000, TrovoSpellCurrency.ELIXIR),
     STAY_SAFE("https://static.trovo.live/imgupload/shop/20200421_tyxzwfzebxbhelmet.png", "Stay Safe", 100, TrovoSpellCurrency.MANA),
     ON_FIRE("https://static.trovo.live/imgupload/shop/20200610_4ig5dz712a20200610_i74sm6yafscannon2.png", "On Fire", 500, TrovoSpellCurrency.MANA),
